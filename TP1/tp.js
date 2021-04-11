ef3d4881110e0dbccde35c812ab60d081c052a6f
Guillestations <guillejuanmanuel@gmail.com>
2021-04-11 20:07:22 -0300

    Tp renderizado

--- TP1/tp.js
+++ TP1/tp.js
@@ -20,15 +20,39 @@ console.log(contarVocales('1 a234'));
 Crear un repo en github y subir todo el proyecto. Se ignorará la carpeta node_modules (para ellos está creado el archivo .gitignore en este proyecto) Esta función devolverá un string con la url del repo.
 */
 function urlRepo() {
-  return
+  const url= 'https://github.com/Guillestations/NodeIt.git';
+  
+  return url;
 }
 
 /* 
 Crear una función arrow, que devuelva una clase en ES6 que contendrá dos métodos llamados contadorPalabras y hayNumeros. La clase recibirá un texto que se guardará en una propiedad llamada texto. contadorPalabras retornará la cantidad de palabras encontradas en la propiedad texto y hayNumeros devolverá true en caso de que encuentre un número en dicho texto, caso contrario retorna false. En ambos métodos, si el texto no es válido, se devolverá -1
 Crear un propiedad estática contadorInstancias que me indique cuantas instancias hay de esa clase.
 */
-const crearClase = () => {
-  return
+const crearClase = () => class Retorno {
+  constructor(texto){
+    this.texto = (typeof (texto) === 'string'? texto : -1);
+    Retorno.contadorInstancias = (Retorno.contadorInstancias || 0 ) +1;
+  }
+    
+  contadorPalabras(){
+    if(typeof(this.texto) === 'string'){
+      const palabras = this.texto.trim().split(' ');
+      let contador = palabras.filter(x => x === ' ');
+      return contador.length+1;
+    }else{
+      return -1
+    }
+  }
+  hayNumeros(){
+    if(typeof(this.texto) === 'string'){
+      const regex = /\'0-9'/g;
+      const number = this.texto.match(regex);
+      return (number !== null)? true: false;
+    }
+      
+  }
+  return 
 }
 
 module.exports = {
